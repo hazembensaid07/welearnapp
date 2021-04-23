5c49a8cef1b2cb6cf1d188e6aa63668b21959d44
hazembensaid07 <58050302+hazembensaid07@users.noreply.github.com>
2021-04-23 13:26:36 +0200

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+const path = require("path");
 const app = express();
 
 // connect to db
@@ -43,7 +43,12 @@ app.use("/api", courseRoutes);
 app.use("/api", blogRoutes);
 app.use("/api", commentsRoutes);
 app.use("/api", coursenrollRoutes);
-
+// serve static assets if an production
+if (process.env.NODE_ENV === "production")
+  app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
